ad5142672faa64a94a0fb6042bb5ba2f9ef1da1f
andy tham <thamandyy@gmail.com>
2017-12-07 23:25:47 -0500

    figured out how to turn a string into a variable

--- js/main.js
+++ js/main.js
@@ -17,10 +17,46 @@ col = ['a','b'];
   // }
   // let test = document.createElement('img');
   // test.setAttribute('src','images/bishop.png');
+
+class ChessPiece {
+  constructor(type, color, posX, posY, isAlive){
+    this.type = type;
+    this.color = color;
+    this.posX = posX;
+    this.posY = posY;
+    this.isAlive = isAlive;
+  }
+  pushIntoArray(){
+    eval(this.color+"Pieces").push(this);
+    //https://stackoverflow.com/questions/5613834/convert-string-to-variable-name-in-javascript
+    //includes other methods instead of eval();
+  }
+}
+
+
+const whitePieces = [];
+
+const createPawns = function createBothPlayersPawns(){
+  const whitePawn1 = new ChessPiece("pawn", "white", 1, 2, true);
+  const whitePawn2 = new ChessPiece("pawn", "white", 2, 2, true);
+  const whitePawn3 = new ChessPiece("pawn", "white", 3, 2, true);
+  const whitePawn4 = new ChessPiece("pawn", "white", 4, 2, true);
+  const whitePawn5 = new ChessPiece("pawn", "white", 5, 2, true);
+  const whitePawn6 = new ChessPiece("pawn", "white", 6, 2, true);
+  const whitePawn7 = new ChessPiece("pawn", "white", 7, 2, true);
+  const whitePawn8 = new ChessPiece("pawn", "white", 8, 2, true);
+  for(i = 1; i < 9; i++){
+    eval("whitePawn"+i).pushIntoArray();
+    eval("blackPawn"+i).pushIntoArray();
+  }
+  console.log(whitePieces);
+}
+
+
 const $board = $('#board');
 
-let createBoard = function createChessBoard(){
-  for(i = 0; i < col.length; i++){
+const createBoard = function createChessBoard(){
+  for(i = 1; i < col.length+1; i++){
     let $colCreate = $("<div>", {"class": "col col" + i});
     $board.append($colCreate);
     let $grabCurrentCol = $(".col" + i);
@@ -32,6 +68,11 @@ let createBoard = function createChessBoard(){
   }
 }
 
+const initBoard = function initalizeChessBoard(){
+  return;
+}
 
 createBoard();
+createPawns();
+initBoard();
 }); //end of jquery
