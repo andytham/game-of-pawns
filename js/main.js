bfda8184187226c937e37be5c6d10d32dd11faf2
andy tham <thamandyy@gmail.com>
2017-12-07 22:36:21 -0500

    create board logic finished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(() => {
 
-
+let row = [];
+let col = [];
+row = ['poop','poop2',54,214,2];
+col = ['a','b'];
   // for(i = 0; i < col.length;i++){
   //   let colCreate = document.createElement('div');
   //   colCreate.setAttribute('class', "col"+i);
@@ -18,9 +21,17 @@ const $board = $('#board');
 
 let createBoard = function createChessBoard(){
   for(i = 0; i < col.length; i++){
-    let $colCreate = $("<div>", {"class": "col" + i});
+    let $colCreate = $("<div>", {"class": "col col" + i});
     $board.append($colCreate);
+    let $grabCurrentCol = $(".col" + i);
+    //create row entries
+    for(j = row.length; j > 0; j--){
+      let $rowCreate = $("<div></div>", {"class": "row col" + i + " row" + j});
+      $grabCurrentCol.append($rowCreate);
+    }
   }
 }
 
+
+createBoard();
 }); //end of jquery
